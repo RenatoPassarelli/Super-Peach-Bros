ea63a9afc5a5009891db61478fb323e522cbf4b7
Beatriz Borges <zackiewiczbeatriz@gmail.com>
2022-05-18 17:16:49 -0300

    teste da janelah

--- Peach.v1.py
+++ Peach.v1.py
@@ -1,1 +1,30 @@
 # Fala mulecadinha, tudo bem? Suave na nave? Partiu pegar uma prainha, com os truta de verdade
+
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((1200, 600))
+pygame.display.set_caption('Hello World!')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
