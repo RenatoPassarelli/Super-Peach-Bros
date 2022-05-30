8fd0d0e311b7e0e9fc76e64806d77bb5e74f1ce9
Felipe Fuchs <felipelf3@al.insper.edu.br>
2022-05-30 17:20:54 -0300

    mapas

---
+++ mapas.py
@@ -0,0 +1,55 @@
+# CONFIGURAÇÕES GERAI
+
+#bex 
+nivel_mapa = [
+    '                           ',
+    '                           ',
+    '           XXX             ',
+    '    XX                     ',
+    '                  XXXXXXX  ',
+    '        XXXXX              ',
+    '                           ',
+    '      XXXXXXXXXXXX  XXXXXXX',
+    ' P XXXXXXXXXXXXXXX  XXXXXXX',
+    'XXXXXXXXXXXXXXXXXX  XXXXXXX'
+]
+
+tile_size = 60
+jan_largura = 1200
+jan_altura = len(nivel_mapa)*tile_size 
+
+#rereee
+nivel_mapa1 = [
+    '                           ',
+    '                           ',
+    '           XXX             ',
+    '    XX                     ',
+    '                  XXXXXXX  ',
+    '        XXXXX              ',
+    '                           ',
+    '      XXXXXXXXXXXX  XXXXXXX',
+    ' P XXXXXXXXXXXXXXX  XXXXXXX',
+    'XXXXXXXXXXXXXXXXXX  XXXXXXX'
+]
+
+tile_size = 60
+jan_largura = 1200
+jan_altura = len(nivel_mapa)*tile_size 
+
+#fefuuu
+nivel_mapa2 = [
+    '                           ',
+    '                           ',
+    '           XXX             ',
+    '    XX                     ',
+    '                  XXXXXXX  ',
+    '        XXXXX              ',
+    '                           ',
+    '      XXXXXXXXXXXX  XXXXXXX',
+    ' P XXXXXXXXXXXXXXX  XXXXXXX',
+    'XXXXXXXXXXXXXXXXXX  XXXXXXX'
+]
+
+tile_size = 60
+jan_largura = 1200
+jan_altura = len(nivel_mapa)*tile_size 
