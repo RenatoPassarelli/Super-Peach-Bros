e7cce1729a70dd1448195394c4498b2c7e73cdb2
Renato Passarelli <renatoguedespassarelli@gmail.com>
2022-05-30 20:31:37 -0300

    Colocando movimentos dentro da classe

--- Peach.v1.py
+++ Peach.v1.py
@@ -23,18 +23,6 @@ peachzinha1=pygame.transform.scale(peachzinha, (60,80))
 peachzinhaco=pygame.image.load(path.join(path.dirname(__file__),"Imagens\Peachzinha contrária.png")).convert_alpha()
 peachzinhaco1=pygame.transform.scale(peachzinhaco, (60,80))
   
-
-
-# ----- Inicia estruturas de dados
-# ----- Plota o chão (futuramente vai ser uma classe)
-# window.blit(chaozinho1, (0, 540)), ----------- acho que podemos apagar tudo q ta aqui 
-# window.blit(chaozinho1, (200, 540)),
-# window.blit(chaozinho1, (400, 540)), 
-# window.blit(chaozinho1, (600, 540)),  
-# window.blit(chaozinho1, (800, 540)),  
-# window.blit(chaozinho1, (1000, 540))
-
-        
 game = True
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
@@ -52,30 +40,7 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
-        # Verifica se apertou alguma tecla. 
-        # Andar   
-        if event.type == pygame.KEYDOWN:
-            if event.key== pygame.K_LEFT:
-                player.speedx -= 6
-            if event.key== pygame.K_RIGHT:
-                player.speedx += 6
-        if event.type == pygame.KEYUP:
-            if event.key== pygame.K_LEFT:
-                player.speedx += 6
-            if event.key== pygame.K_RIGHT:
-                player.speedx -= 6
-        # Verifica se apertou alguma tecla.
-    keys_pressed= pygame.key.get_pressed()
-            # Dependendo da tecla, altera a velocidade.
-    if keys_pressed[pygame.K_SPACE]:
-            jumping= True
-    if jumping:
-        player.rect.y -= y_velocidade
-        y_velocidade-= y_gravidade
-        if y_velocidade < -(y_saltomax):
-            jumping= False
-            y_velocidade= y_saltomax
-
+        
     # Atualizando a posição das sprites
     all_sprites.update()
     # ----- Gera saídas
--- player.py
+++ player.py
@@ -15,7 +15,10 @@ class boneco(pygame.sprite.Sprite):
     def __init__(self, img, imgcont):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
-
+        self.y_gravidade=1
+        self.y_saltomax=20
+        self.y_velocidade=self.y_saltomax
+        self.jumping=False
         self.image = img
         self.certo=img
         self.cont=imgcont
@@ -25,17 +28,33 @@ class boneco(pygame.sprite.Sprite):
         self.speedx = 0
         self.speedy=0
 
-    def update(self):
-        
-        # Atualização da posição da PEACH
+    def get_input(self):
+        #movimento do boneco
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        keys_pressed=pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_SPACE]:
+            self.jumping=True
+        if self.jumping:
+            self.rect.y-=self.y_velocidade
+            self.y_velocidade -= self.y_gravidade
+            if self.y_velocidade <-(self.y_saltomax):
+                self.jumping = False
+                self.y_velocidade = self.y_saltomax
+        if keys_pressed[pygame.K_LEFT]:
+            self.rect.x -= 6
+            self.image= self.cont
+        if keys_pressed[pygame.K_RIGHT]:
+            self.rect.x += 6
+            self.image= self.certo
+
+
+    def update(self):
+
         # Mantem dentro da tela
         if self.rect.right > 1200:
             self.rect.right =1200
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.speedx<0:
-            self.image= self.cont
-        if self.speedx>0:
-            self.image= self.certo
+        self.get_input()
