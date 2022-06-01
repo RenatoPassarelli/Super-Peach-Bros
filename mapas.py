10092f63876b7f3ecf9bf1d8f0c586dc02418eeb
Felipe Fuchs <felipelf3@al.insper.edu.br>
2022-06-01 15:13:49 -0300

    mapa novo

--- mapas.py
+++ mapas.py
@@ -4,17 +4,16 @@ from config import tile_size
 
 MAPAS = {
     0: [
-        '                           ',
-        '                           ',
-        '           XXX             ',
-        '                         ',
-        '                  XXXXXXX  ',
-        '                      ',
-        '                           ',
-        '                            ',
-        '  P XXXX                       ',
-        '    XX XXXXXXX'
-    ],
+    '                                                                                                                       ',
+    '                                                                                                                       ',
+    '                                                                                             XXXX                      ',
+    '                  XXXXXXX            XXXXXXXX          XXX                                                             ',
+    '                                   XXXXXXXXXX    XXX          XXX                     XXXX               X             ',
+    '               X                 XXXXXXXXXXXX                       XXX   XXX                            X             ',
+    '           XXXXXXX   XXXXXX   XXXXXXXXXXXXXXX                                   XXXXXX                 XXXXXXXXXXXXXXXX',
+    ' P                   XXXXXX   XXXXXXXXXXXXXXX                                   XXXXXX                 XXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXX   XXXXXX   XXXXXXXXXXXXXXX                                   XXXXXX                 XXXXXXXXXXXXXXXX'
+] ,
     2: [
         '                           ',
         '                           ',
@@ -41,4 +40,4 @@ MAPAS = {
     ]
 }
 jan_largura = 1200
-jan_altura = len(MAPAS[0])*tile_size 
+jan_altura = len(MAPAS[0])*tile_size
