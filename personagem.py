d629d4f49040a2f005e4d2f50218febcf424b295
Beatriz Borges <zackiewiczbeatriz@gmail.com>
2023-03-16 17:29:21 -0300

    criando função de colisão

--- personagem.py
+++ personagem.py
@@ -10,6 +10,24 @@ JUMPING=1
 FALLING=2
 GRAVIDADE=2
 
+# Função colisão com blocos
+def colisao_blocos(classe, self, bloco):
+    if classe == "Player": 
+        for blocos in bloco:
+            if self.speedx > 0:
+                self.rect.right = blocos.rect.left
+                return self.rect.right 
+            elif self.speedx < 0:
+                self.rect.left = blocos.rect.right
+                return self.rect.left
+    elif classe == "Gombas": 
+        for blocos in bloco:
+            if self.speedx > 0:
+                self.speedx=-self.speedx
+            elif self.speedx < 0:
+                self.speedx=-self.speedx
+
+
 # Cria Classe do personagem
 class Player(pygame.sprite.Sprite):
     def __init__(self,img,imgco,l,c,peças,ind):
@@ -60,11 +78,8 @@ class Player(pygame.sprite.Sprite):
         
         # Collide com os blocos 
         bloco = pygame.sprite.spritecollide(self, self.blocoss, False)
-        for blocos in bloco:
-            if self.speedx > 0:
-                self.rect.right = blocos.rect.left
-            elif self.speedx < 0:
-                self.rect.left = blocos.rect.right
+        classe = "Player"
+        colisao_blocos(classe, self, bloco)
 
     def jump(self): # Pular 
         if self.state == STILL:
@@ -96,11 +111,8 @@ class Gombas(pygame.sprite.Sprite):
         if self.frame_atual > len(self.frames):
             self.frame_atual = 0
         self.image = self.frames[int(self.frame_atual)]
-        for blocos in limitess:
-            if self.speedx > 0:
-                self.speedx=-self.speedx
-            elif self.speedx < 0:
-                self.speedx=-self.speedx
+        classe = "Gombas"
+        colisao_blocos(classe, self, limitess)
 
     def afundando(self):
         self.image = pygame.image.load(path.join('Super-Peach-Bros/Imagens/goompa2/Imagem{}.png'.format(3))).convert_alpha()
